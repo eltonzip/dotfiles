df0e57a9789e9fb8d4d5bb4be9ddce28d4d4b6b7
Egor Averyanov <egor.a2004@gmail.com>
2025-04-22 15:42:02 +0300

    Refactored some tmux stuff

--- Scripts/copy-cfg-to-repo.sh
+++ Scripts/copy-cfg-to-repo.sh
@@ -3,7 +3,7 @@
 cp $HOME/.bashrc $HOME/dotfiles/bashrc
 cp $HOME/.vimrc $HOME/dotfiles/vimrc
 cp $HOME/.gitconfig $HOME/dotfiles/gitconfig
-cp $HOME/.config/tmux/* $HOME/dotfiles/tmux/
+cp $HOME/.tmux.conf $HOME/dotfiles/tmux.conf
 cp $HOME/.config/alacritty/* $HOME/dotfiles/alacritty/
 cp $HOME/Scripts/* $HOME/dotfiles/Scripts/
 cp $HOME/.config/nvim/* $HOME/dotfiles/nvim/
