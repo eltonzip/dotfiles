2b23b59be31a241912d1aeb2e3933d4dbcfc5edd
Egor Averyanov <egor.a2004@gmail.com>
2025-04-24 17:46:45 +0300

    hehe

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,6 @@ su --command='apt install opendoas && cp doas.conf /etc/' root
 
 doas apt install -y build-essential tmux vim ripgrep gdb universal-ctags htop
 
-mkdir $HOME/.config
-
 cp bashrc $HOME/.bashrc
 cp tmux.conf $HOME/.tmux.conf
 cp -r Scripts $HOME/
