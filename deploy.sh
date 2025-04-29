d06f31bbbdf0ab6bd0f6cc1f24779d83fee73ed1
Egor Averyanov <egor.a2004@gmail.com>
2025-04-29 16:03:54 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-sudo apt install -y build-essential tmux vim ripgrep gdb htop alacritty
+sudo apt install -y build-essential tmux vim ripgrep gdb htop alacritty clang clangd curl
 
 cp bash_ez $HOME/.bash_ez
 cp tmux.conf $HOME/.tmux.conf
