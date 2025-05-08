fb7714cea48cb54239cae2d70459269d3855832b
Egor Averyanov <egor.a2004@gmail.com>
2025-05-08 18:59:16 +0300

    improved deployment

--- deploy.sh
+++ deploy.sh
@@ -9,3 +9,9 @@ cp vimrc $HOME/.vimrc
 cp -r nvim $HOME/.config/
 cp gitconfig $HOME/.gitconfig
 cp -r alacritty $HOME/.config/
+
+sed -i '$a\
+\
+if [ -f $HOME/.bash_ez ]; then\
+	. $HOME/.bash_ez\
+fi' $HOME/.bashrc
