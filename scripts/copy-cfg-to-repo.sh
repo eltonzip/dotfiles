5bb9d522d95c2230001c0c074a4a474186acbc62
Egor Averyanov <egor.a2004@gmail.com>
2025-05-13 18:28:31 +0300

    gdbinit

--- scripts/copy-cfg-to-repo.sh
+++ scripts/copy-cfg-to-repo.sh
@@ -4,5 +4,6 @@ cp $HOME/.bash_ez $HOME/dotfiles/bash_ez
 cp $HOME/.vimrc $HOME/dotfiles/vimrc
 cp $HOME/.gitconfig $HOME/dotfiles/gitconfig
 cp $HOME/.tmux.conf $HOME/dotfiles/tmux.conf
+cp $HOME/.gdbinit $HOME/dotfiles/.gdbinit
 cp $HOME/scripts/* $HOME/dotfiles/scripts/
 cp $HOME/.config/alacritty/* $HOME/dotfiles/alacritty/
