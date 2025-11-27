9f66f2b004c0c1cb5a6ee45d119f91e6b87a3869
Egor Averyanov <egor.a2004@gmail.com>
2025-11-27 12:32:24 +0300

    apt makes no sense, going back to arch

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,31 @@
 #!/bin/env bash
 
-sudo apt install -y vim tmux gdb build-essential universal-ctags
+if [[ -z $(command -v pacman) ]]; then
+	echo 'no pacman on this machine'
+	exit 1
+fi
 
-ln -s $(pwd)/bash_ez    $HOME/.bash_ez
-ln -s $(pwd)/vimrc      $HOME/.vimrc
-ln -s $(pwd)/tmux.conf  $HOME/.tmux.conf
-ln -s $(pwd)/gitconfig  $HOME/.gitconfig
-ln -s $(pwd)/gdbinit    $HOME/.gdbinit
+if [[  -n $1 && $1 == 'gui' ]]; then
+	sudo pacman -S firefox libreoffice-fresh pavucontrol             \
+		noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra  \
+		foot mpv
 
-sed -i '$a\
+	ln -s $(pwd)/gui/foot    $HOME/.config/foot
+	ln -s $(pwd)/gui/mpv     $HOME/.config/mpv
+else
+	sudo pacman -S needed tmux gdb ctags
+
+	ln -s $(pwd)/cli/bash_ez    $HOME/.bash_ez
+	ln -s $(pwd)/cli/vimrc      $HOME/.vimrc
+	ln -s $(pwd)/cli/tmux.conf  $HOME/.tmux.conf
+	ln -s $(pwd)/cli/gitconfig  $HOME/.gitconfig
+	ln -s $(pwd)/cli/gdbinit    $HOME/.gdbinit
+fi
+
+grep 'bash_ez' $HOME/.bashrc &>/dev/null
+if [[ $? != 0 ]]; then
+	sed -i '$a\
 \
 #eltonzip:\
 [[ -L $HOME/.bash_ez ]] && . $HOME/.bash_ez' $HOME/.bashrc
+fi
